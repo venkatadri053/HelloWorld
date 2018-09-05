6d76a1ad07f2c57d4f5d85f68d96a9b77b6acab9
rs <rs@DESKTOP-V0HG1DG>
2018-09-05 20:59:07 +0530

    subtraction

--- src/com/app/AddTwoIntegers.java
+++ src/com/app/AddTwoIntegers.java
@@ -7,8 +7,8 @@ public class AddTwoIntegers {
         int first = 10;
         int second = 20;
 
-        int sum = first + second;
+        int sub = first + second;
 
-        System.out.println("The sum is: " + sum);
+        System.out.println("The sub is: " + sub);
     }
 }
