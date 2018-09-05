b6f3fb6b529316e4ae476be4a774b75a500a8d36
KESANI.VENKATADRI <35061636+venkatadri053@users.noreply.github.com>
2018-09-05 21:28:37 +0530

    Update Sample.java

--- src/com/app/Sample.java
+++ src/com/app/Sample.java
@@ -5,7 +5,11 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
 		
-		
+		System.out.println("v");
+		System.out.println("e");
+		System.out.println("n");
+		System.out.println("k");
+		System.out.println("y");
 		
 		System.out.println("Hai");
 	}
